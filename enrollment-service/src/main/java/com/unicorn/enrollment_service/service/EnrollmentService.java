fb693cf5c20dfb11f01adc67e85eee2c742b9892
raafi-4z1 <raafia452@gmail.com>
2024-08-30 16:20:31 +0700

    rev Get Enrollment by Pagination

--- enrollment-service/src/main/java/com/unicorn/enrollment_service/service/EnrollmentService.java
+++ enrollment-service/src/main/java/com/unicorn/enrollment_service/service/EnrollmentService.java
@@ -1,7 +1,11 @@
 package com.unicorn.enrollment_service.service;
 
+import com.unicorn.enrollment_service.dto.EnrollmentDto;
 import com.unicorn.enrollment_service.dto.InsertEnrollmentDto;
+
+import java.util.List;
 
 public interface EnrollmentService {
     String insertEnrollment(InsertEnrollmentDto dto);
+    List<EnrollmentDto> getEnrollmentByPagination(Integer page);
 }
