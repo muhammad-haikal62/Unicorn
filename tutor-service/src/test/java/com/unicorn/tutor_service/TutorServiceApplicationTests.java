d2d5890f00b76754777d621822c037d3cbf17585
hasimsaputro <179096240+hasimsaputro@users.noreply.github.com>
2024-08-30 14:00:20 +0700

    Add Tutor Service

---
+++ tutor-service/src/test/java/com/unicorn/tutor_service/TutorServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.unicorn.tutor_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TutorServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
