027ce1f8616169da69dae75f62484776b01e25d4
martin220300 <75156315+martin220300@users.noreply.github.com.>
2024-08-30 15:52:03 +0700

    edit MajorController

--- subject-service/src/main/java/com/unicorn/subject_service/controller/MajorController.java
+++ subject-service/src/main/java/com/unicorn/subject_service/controller/MajorController.java
@@ -2,10 +2,16 @@ package com.unicorn.subject_service.controller;
 
 import com.unicorn.subject_service.dto.MajorGrid;
 import com.unicorn.subject_service.dto.UpdateInsertMajor;
+import com.unicorn.subject_service.entity.Major;
 import com.unicorn.subject_service.service.MajorService;
+import jakarta.ws.rs.GET;
+import jakarta.ws.rs.Path;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("major")
@@ -19,8 +25,33 @@ public class MajorController {
     @GetMapping("")
     public ResponseEntity<Object> getMajors(){
         try{
-            MajorGrid majorGrid =
+            List<MajorGrid> majors = service.getMajors();
+            return ResponseEntity.status(HttpStatus.OK).body(majors);
         }catch (Exception ex){
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(ex.getMessage());
+        }
+    }
+
+    @GetMapping("/{majorId}")
+    public ResponseEntity<Object> getMajorById(
+            @PathVariable Integer majorId
+    ){
+        try{
+            MajorGrid majorGrid = service.getMajorById(majorId);
+            return ResponseEntity.status(HttpStatus.OK).body(majorGrid);
+        }catch (Exception ex){
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(ex.getMessage());
+        }
+    }
+
+    @GetMapping("editMajor")
+    public ResponseEntity<Object> editMajor(
+            @RequestParam(required = false) Integer majorId
+    ){
+        try {
+            UpdateInsertMajor updateInsertMajor = service.editMajor(majorId);
+            return ResponseEntity.status(HttpStatus.OK).body(updateInsertMajor);
+        } catch (Exception ex){
             return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(ex.getMessage());
         }
     }
