6317d614d87604e96f7857a8210d4f4b7e824827
winangsit12 <134899986+winangsit12@users.noreply.github.com>
2024-08-30 15:29:27 +0700

    Add Enrollment

---
+++ enrollment-service/src/main/java/com/unicorn/enrollment_service/repository/EnrollmentRepository.java
@@ -0,0 +1,9 @@
+package com.unicorn.enrollment_service.repository;
+
+import com.unicorn.enrollment_service.entity.Enrollment;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface EnrollmentRepository extends JpaRepository<Enrollment, Integer> {
+}
