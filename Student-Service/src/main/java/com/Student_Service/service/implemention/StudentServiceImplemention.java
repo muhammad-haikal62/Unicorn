aac09940d35d43ab5153db0d62421e9885ac670e
elisaawardhani <113404545+elisaawardhani@users.noreply.github.com>
2024-08-30 16:21:17 +0700

    add method (student service) & add entity major

--- Student-Service/src/main/java/com/Student_Service/service/implemention/StudentServiceImplemention.java
+++ Student-Service/src/main/java/com/Student_Service/service/implemention/StudentServiceImplemention.java
@@ -2,8 +2,12 @@ package com.Student_Service.service.implemention;
 
 import com.Student_Service.dto.CertificateDto;
 import com.Student_Service.dto.StudentDto;
+import com.Student_Service.dto.StudentMajorDto;
 import com.Student_Service.dto.StudentInsertDto;
 import com.Student_Service.entity.Student;
+import com.Student_Service.entity.StudentMajor;
+import com.Student_Service.repository.MajorRepository;
+import com.Student_Service.repository.StudentMajorRepository;
 import com.Student_Service.repository.CertificateRepository;
 import com.Student_Service.repository.StudentRepository;
 import com.Student_Service.service.StudentService;
@@ -19,12 +23,16 @@ import java.util.List;
 @Service
 public class StudentServiceImplemention implements StudentService {
     private final StudentRepository repository;
+    private final StudentMajorRepository studentMajorRepository;
+    private final MajorRepository majorRepository;
     private final CertificateRepository certificateRepository;
-    
+
 
     @Autowired
-    public StudentServiceImplemention(StudentRepository repository, CertificateRepository certificateRepository) {
+    public StudentServiceImplemention(StudentRepository repository, StudentMajorRepository studentMajorRepository, MajorRepository majorRepository, CertificateRepository certificateRepository) {
         this.repository = repository;
+        this.studentMajorRepository = studentMajorRepository;
+        this.majorRepository = majorRepository;
         this.certificateRepository = certificateRepository;
     }
 
@@ -168,6 +176,21 @@ public class StudentServiceImplemention implements StudentService {
         var student = repository.findById(studentNumber).orElseThrow();
         repository.delete(student);
         return true;
+    }
+
+    @Override
+    public List<StudentMajorDto> getMajorByStudentNumber(String studentNumber) {
+        var majorsList = new LinkedList<StudentMajorDto>();
+        for(var major : studentMajorRepository.getMajorByStudentNumber(studentNumber)) {
+            var majorDetail = majorRepository.findById(major.getMajorId()).orElseThrow();
+            var majors = new StudentMajorDto();
+            majors.setName(majorDetail.getName());
+            majors.setType(majorDetail.getType());
+            majors.setLevel(majorDetail.getLevel());
+            majors.setDescription(majorDetail.getDescription());
+            majorsList.add(majors);
+        }
+        return majorsList;
     }
 
     @Override
