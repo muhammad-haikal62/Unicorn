17a782092daadd776e7cb35f09f8e8f9fcfcadd9
Haikal Hidayatullah <83757209+HaikalHdytullah@users.noreply.github.com>
2024-09-02 16:27:56 +0700

    restful api location

--- Location-Service/src/main/java/com/unicorn/location_service/controller/CityController.java
+++ Location-Service/src/main/java/com/unicorn/location_service/controller/CityController.java
@@ -12,7 +12,7 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.List;
 
 @RestController
-@RequestMapping("city")
+@RequestMapping("api/city")
 public class CityController {
     private final CityService service;
 
--- Location-Service/src/main/java/com/unicorn/location_service/controller/CountryController.java
+++ Location-Service/src/main/java/com/unicorn/location_service/controller/CountryController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("country")
+@RequestMapping("api/country")
 public class CountryController {
     private final CountryService service;
 
